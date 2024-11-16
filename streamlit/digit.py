caf661d26a4ba92924ad4ace79aa4a94ef96cbac
usmonaliyev99 <t.usmonaliyev99@gmail.com>
2024-11-16 16:24:22 +0500

    feat: streamlit is added to project with digit prediction

---
+++ streamlit/digit.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from fastai.vision.all import PILImage, load_learner
+
+model = load_learner("/home/t1nnur/.fastai/data/mnist_png/mnist.pkl")
+
+st.title("Digit prediction")
+
+print(type(model))
+
+
+def main():
+    file = st.file_uploader("Upload your file:")
+
+    if not file:
+        return
+
+    img = PILImage.create(file)
+
+    prediction, predict_id, percents = model.predict(img)
+
+    st.success(f"Prediction: {prediction}")
+    st.text(f"Percent: {percents[predict_id]}")
+
+    st.image(img)
+
+
+main()
